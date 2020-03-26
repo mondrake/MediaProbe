3a330a4638c4ba6d337693086b1cff5fc4cd6dd3
mondrake <mondrake.org@gmail.com>
2020-03-26 12:23:51 +0100

    c

--- src/Block/Jpeg.php
+++ src/Block/Jpeg.php
@@ -47,6 +47,7 @@ class Jpeg extends BlockBase
                 $offset = $this->getJpegSegmentIdOffset($data_element, $offset);
             }
             catch (DataException $e) {
+dump([MediaProbe::dumpHex($data_element->getBytes(), 50)]);
                 $this->error($e->getMessage());
                 $this->valid = false;
                 return;
