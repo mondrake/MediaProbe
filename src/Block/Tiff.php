046fc4b6bc390b38b42e5b2c13ff5b8ee3a7b694
mondrake <mondrake.org@gmail.com>
2020-04-06 10:34:37 +0200

    9

--- src/Block/Tiff.php
+++ src/Block/Tiff.php
@@ -82,7 +82,8 @@ class Tiff extends BlockBase
                 $ifd_item = new ItemDefinition($this->getCollection()->getItemCollection($i), ItemFormat::LONG, $ifd_tags_count, $ifd_offset, 0, $i);
                 $ifd = new $ifd_class($ifd_item, $this);
 //                $ifd_data_window = new DataWindow($data_element, $ifd_offset);
-                $ifd->loadFromData($data_element, $ifd_offset + 4);
+dump(MediaProbe::dumpHex($data_element->getBytes($ifd_offset, 50)));
+                $ifd->loadFromData($data_element, $ifd_offset);
 //                $ifd->loadFromData($ifd_data_window);
 
                 // Offset to next IFD.
