a59eafc6df7141dbce16485d646b65d9c11187b6
mondrake <mondrake.org@gmail.com>
2020-04-12 14:46:56 +0200

    11

--- src/Block/JpegSegmentSos.php
+++ src/Block/JpegSegmentSos.php
@@ -7,6 +7,7 @@ use FileEye\MediaProbe\Data\DataElement;
 use FileEye\MediaProbe\Data\DataWindow;
 use FileEye\MediaProbe\Entry\Core\Undefined;
 use FileEye\MediaProbe\ItemDefinition;
+use FileEye\MediaProbe\ItemFormat;
 use FileEye\MediaProbe\Utility\ConvertBytes;
 
 /**
