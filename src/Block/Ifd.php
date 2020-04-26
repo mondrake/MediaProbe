02498e69f3c8118c68b1266d2296f5d7f199c2af
mondrake <mondrake.org@gmail.com>
2020-04-26 09:23:55 +0200

    2

--- src/Block/Ifd.php
+++ src/Block/Ifd.php
@@ -46,7 +46,11 @@ class Ifd extends ListBase
                     $item->loadFromData($data_element);
                 }
                 else {
-                    $item_data_window = new DataWindow($data_element, $item_definition->getDataOffset(), $item_definition->getSize());
+                    // In case of an IFD terminator item entry, i.e. zero
+                    // components, the data window size is still 4 bytes, from
+                    // the IFD index area.
+                    $item_data_window_size = $item_definition->getValuesCount() === 0 ? $item_definition->getSize() : 4;
+                    $item_data_window = new DataWindow($data_element, $item_definition->getDataOffset(), $item_data_window_size);
                     $item->loadFromData($item_data_window);
                 }
             } catch (DataException $e) {
@@ -116,7 +120,7 @@ class Ifd extends ListBase
         $format = $data_element->getShort($offset + 2);
         $components = $data_element->getLong($offset + 4);
         $size = ItemFormat::getSize($format) * $components;
-if ($components == 0) dump([$id, $format, $components, $size]);
+//if ($components == 0) dump([$id, $format, $components, $size]);
 
         // If the data size is bigger than 4 bytes, then actual data is not in
         // the TAG's data element, but at the the offset stored in the data
