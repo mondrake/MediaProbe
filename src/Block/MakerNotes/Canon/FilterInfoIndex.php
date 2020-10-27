88c1a18ba2a679a08c70d876be5436d3ab157aa8
mondrake <mondrake.org@gmail.com>
2020-10-27 17:18:53 +0100

    a1

--- src/Block/MakerNotes/Canon/FilterInfoIndex.php
+++ src/Block/MakerNotes/Canon/FilterInfoIndex.php
@@ -14,7 +14,19 @@ use FileEye\MediaProbe\Utility\ConvertBytes;
 /**
  * Class representing an index of values, for Canon Filter information.
  *
- * The actual map collection need to be resolved.
+ * Data segment structure:
+ *
+ * Header   Count
+ * D4000000 07000000
+ *
+ * Id       Lenght   P count  P#1 Idx  P#1 cnt  P#1 val  P#2 Idx  P#2 cnt  P#2 val  ...
+ * 01000000 14000000 01000000 01010000 01000000 FFFFFFFF
+ * 02000000 14000000 01000000 01020000 01000000 FFFFFFFF
+ * 03000000 14000000 01000000 01030000 01000000 FFFFFFFF
+ * 04000000 38000000 04000000 01040000 01000000 FFFFFFFF 02040000 01000000 00000000 03040000 01000000 00000000 04040000 01000000 00000000
+ * 05000000 14000000 01000000 01050000 01000000 FFFFFFFF
+ * 06000000 14000000 01000000 01060000 01000000 FFFFFFFF
+ * 07000000 14000000 01000000 01070000 01000000 FFFFFFFF
  */
 class FilterInfoIndex extends Index
 {
@@ -28,14 +40,31 @@ class FilterInfoIndex extends Index
 //        $this->validate($data_element);
 
         $offset = 0;
-        $index_components = $data_element->getLong($offset + 4);
-        $this->debug("Components: $index_components");
-        $offset += 8;
+
+        // The first 4 bytes are markers, store in a RawData.
+        $header_data_definition = new ItemDefinition(Collection::get('RawData', ['name' => 'filterHeader']), ItemFormat::BYTE, 4);
+        $header_data_window = new DataWindow($data_element, $offset, 4);
+        $header = new RawData($header_data_definition, $this);
+        $header->parseData($header_data_window);
+
+        // The next 4 bytes define the count of filters.
+        $offset += 4;
+        $index_components = $data_element->getLong($offset);
+        $this->debug("{filters} filters", [
+            'filters' => $index_components,
+        ]);
+
+        // Loop through the filters.
+        $offset += 4;
         for ($i = 0; $i < $index_components; $i++) {
             $filter_number = $data_element->getLong($offset);
             $filter_size = $data_element->getLong($offset + 4);
             $filter_param_count = $data_element->getLong($offset + 8);
-            $this->debug("Filter: $filter_number $filter_size $filter_param_count");
+            $this->debug("Filter {filter}, {params} parameter(s), size {size} bytes", [
+                'filter' => $filter_number,
+                'params' => $filter_param_count,
+                'size' => $filter_size,
+            ]);
             $next = $offset + 4 + $filter_size;
             $offset += 12;
             for ($p = 0; $p < $filter_param_count; $p++) {
@@ -44,15 +73,18 @@ class FilterInfoIndex extends Index
                 $offset += 8;
                 $val = $data_element->getSignedLong($offset);
                 $this->debug("Tag: $id $count $val");
+                try {
+                    $item_definition = new ItemDefinition($this->getCollection()->getItemCollection($id), ItemFormat::SIGNED_LONG, $count);
+                    $class = $item_definition->getCollection()->getPropertyValue('class');
+                    $param = new $class($item_definition, $this);
+                    $param->parseData($data_element, $offset, $count * ItemFormat::getSize(ItemFormat::SIGNED_LONG));
+                } catch (\Exception $e) {
+                    $this->valid = false;
+                    $this->error($e->getMessage());
+                    throw new MediaProbeException($e->getMessage()); // @todo ingest in logging
+                }
                 $offset += 4 * $count;
             }
-/*            // Adds the 'tag'.
-            $item_class = $item_definition->getCollection()->getPropertyValue('class');
-            $item = new $item_class($item_definition, $this);
-
-            $entry_class = $item_definition->getEntryClass();
-            new $entry_class($item, $this->getValueFromData($data_element, $offset, $item_definition->getFormat(), $value_components));
-            $item->valid = true;*/
             $offset = $next;
         }
 
@@ -62,40 +94,4 @@ class FilterInfoIndex extends Index
         $this->executePostLoadCallbacks($data_element);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function debugBlockInfo(?DataElement $data_element = null, int $items_count = 0)
-    {
-        $msg = '#{seq} {node}:{name}';
-        $seq = $this->getDefinition()->getSequence() + 1;
-        if ($this->getParentElement() && ($parent_name = $this->getParentElement()->getAttribute('name'))) {
-            $seq = $parent_name . '.' . $seq;
-        }
-        $node = $this->DOMNode->nodeName;
-        $name = $this->getAttribute('name');
-        $item = $this->getAttribute('id');
-        if ($item ==! null) {
-            $msg .= ' ({item})';
-        }
-        if (is_numeric($item)) {
-            $item = $item . '/0x' . strtoupper(dechex($item));
-        }
-        if ($data_element instanceof DataWindow) {
-            $msg .= ' @{offset}, {tags} entries, f {format}, s {size}';
-            $offset = $data_element->getAbsoluteOffset() . '/0x' . strtoupper(dechex($data_element->getAbsoluteOffset()));
-        } else {
-            $msg .= ' {tags} entries, format ?xxx, size {size}';
-        }
-        $this->debug($msg, [
-            'seq' => $seq,
-            'node' => $node,
-            'name' => $name,
-            'item' => $item,
-            'offset' => $offset ?? null,
-            'tags' => $this->getDefinition()->getValuesCount(),
-            'format' => ItemFormat::getName($this->getDefinition()->getFormat()),
-            'size' => $this->getDefinition()->getSize(),
-        ]);
-    }
 }
