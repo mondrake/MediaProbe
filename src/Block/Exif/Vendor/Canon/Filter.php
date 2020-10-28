6b9de317346466bd7b99bf49fe6d81c46f162128
mondrake <mondrake.org@gmail.com>
2020-10-28 12:31:00 +0100

    b5

--- src/Block/Exif/Vendor/Canon/Filter.php
+++ src/Block/Exif/Vendor/Canon/Filter.php
@@ -12,6 +12,7 @@ use FileEye\MediaProbe\Data\DataElement;
 use FileEye\MediaProbe\Data\DataWindow;
 use FileEye\MediaProbe\ItemDefinition;
 use FileEye\MediaProbe\ItemFormat;
+use FileEye\MediaProbe\MediaProbe;
 use FileEye\MediaProbe\MediaProbeException;
 use FileEye\MediaProbe\Utility\ConvertBytes;
 
