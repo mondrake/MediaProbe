4d07adca9bd4bac6504bfe696bc1d4c0e33604ad
mondrake <mondrake.org@gmail.com>
2020-10-30 20:58:07 +0100

    9

--- src/Block/JpegSegmentSos.php
+++ src/Block/JpegSegmentSos.php
@@ -40,14 +40,14 @@ class JpegSegmentSos extends JpegSegmentBase
 
         // Append the EOI.
         $end_offset = $scan_size;
-/*        $eoi_collection = ;
+        $eoi_collection = $this->getParentElement()->getCollection()->getItemCollection(self::JPEG_EOI);
         $eoi_class = $eoi_collection->getPropertyValue('class');
         $eoi = new $eoi_class($eoi_collection, $this->getParentElement());
         $eoi_data_window = new DataWindow($data, $end_offset, 2);
-        $eoi->parseData($eoi_data_window);*/
-        $this
+        $eoi->parseData($eoi_data_window);
+/*        $this
             ->addItemWithDefinition(new ItemDefinition($this->getParentElement()->getCollection()->getItemCollection(self::JPEG_EOI), ItemFormat::BYTE, $end_offset))
-            ->parseData($data, $end_offset, 2);
+            ->parseData($data, $end_offset, 2);*/
         $end_offset += 2;
 
         // Now check to see if there are any trailing data.
