653d1958417de96116a80c1e56b22a9151c927e8
mondrake <mondrake.org@gmail.com>
2020-11-05 12:09:46 +0100

    a8

--- src/Entry/ExifExposureBiasValue.php
+++ src/Entry/ExifExposureBiasValue.php
@@ -15,6 +15,6 @@ class ExifExposureBiasValue extends SignedRational
     public function toString(array $options = [])
     {
         $val = $this->getValue();
-        return $val[0] = 0 ? '0' : sprintf('%s%.01f', $val[0] * $val[1] > 0 ? '+' : '', $val[0] / $val[1]);
+        return $val[0] == 0 ? '0' : sprintf('%s%.01f', $val[0] * $val[1] > 0 ? '+' : '', $val[0] / $val[1]);
     }
 }
