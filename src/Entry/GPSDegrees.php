bcd16eec2a8a613846c1e3ad1c9215310cb24219
mondrake <mondrake.org@gmail.com>
2021-08-30 16:17:29 +0200

    1

--- src/Entry/GPSDegrees.php
+++ src/Entry/GPSDegrees.php
@@ -32,6 +32,10 @@ class GPSDegrees extends Rational
         $degrees = $this->getValue()[0][0] / $this->getValue()[0][1];
         $minutes = $this->getValue()[1][0] / $this->getValue()[1][1];
         $seconds = $this->getValue()[2][0] / $this->getValue()[2][1];
-        return sprintf('%s° %s\' %s" (%.2f°)', $degrees, $minutes, $seconds, $degrees + $minutes / 60 + $seconds / 3600);
+        if ($format === 'exiftool') {
+            return sprintf('%s deg %s\' %.2f"', $degrees, $minutes, $seconds);
+        } else {
+            return sprintf('%s° %s\' %s" (%.2f°)', $degrees, $minutes, $seconds, $degrees + $minutes / 60 + $seconds / 3600);
+        }
     }
 }
