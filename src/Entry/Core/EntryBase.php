de881b7345b3b07f20577346ad70dd183b98fd08
mondrake <mondrake.org@gmail.com>
2021-08-31 15:21:32 +0200

    12

--- src/Entry/Core/EntryBase.php
+++ src/Entry/Core/EntryBase.php
@@ -174,6 +174,7 @@ abstract class EntryBase extends ElementBase implements EntryInterface
         if ($null_on_missing) {
             return null;
         }
+if (!is_scalar($value)) dump($value);
         return is_scalar($value) ? $value : implode(' ', $value);
     }
 
