973cea4a25f893d4851c25725693a372e1cd39c4
mondrake <mondrake.org@gmail.com>
2021-08-31 11:35:05 +0200

    i

---
+++ src/Entry/ExifLensInfo.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace FileEye\MediaProbe\Entry;
+
+use FileEye\MediaProbe\Entry\Core\Rational;
+
+/**
+ * Decode text for an Exif/LensInfo tag.
+ */
+class ExifApertureValue extends Rational
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toString(array $options = [])
+    {
+        if (($options['format'] ?? null) === 'exiftool') {
+            $val = $this->getValue($options);
+            if (val[0] == $val[1]) {
+              $str = $val[0];
+            } else {
+              $str = $val[0] . '-'. $val[1];
+            }
+            $str .= ' f/';
+            if (val[2] == 0) {
+              $str .= '?';
+            } elseif (val[2] == $val[3]) {
+              $str .= $val[2];
+            } else {
+              $str .= $val[2] . '-'. $val[3];
+            }
+            return $str;
+        }
+        return parent::toString($options);
+    }
+}
