6a03db60c57645d2d933ce83e92a39c0f4f2df6b
mondrake <mondrake.org@gmail.com>
2021-08-31 18:01:07 +0200

    c

--- src/Entry/ExifExposureTime.php
+++ src/Entry/ExifExposureTime.php
@@ -19,7 +19,7 @@ class ExifExposureTime extends Rational
 
         $sec = $short ? '' : ' sec.';
 
-        if ($this->getValue < 1) {
+        if ($this->getValue() < 1) {
             return MediaProbe::fmt('1/%d%s', $this->getValue(), $sec);
         } else {
             return MediaProbe::fmt('%d%s', $this->getValue(), $sec);
