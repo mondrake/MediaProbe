ce6d54ff49e8042e8a83f2d083c654e76536ae27
mondrake <mondrake.org@gmail.com>
2021-09-03 11:36:21 +0200

    dc

--- test/NumberSignedRationalTest.php
+++ test/NumberSignedRationalTest.php
@@ -41,13 +41,13 @@ class NumberSignedRationalTest extends NumberTestCase
 
         $entry->setValue([[-1, 2], [3, 4], [5, -6]]);
         $this->assertSame([[-1, 2], [3, 4], [5, -6]], $entry->getValue(['format' => 'parsed']));
-        $this->assertSame([-0.5, 0.75, -0.83333333333333], $entry->getValue());
-        $this->assertSame('-0.5 0.75 -0.83333333333333', $entry->toString());
+        $this->assertSame([-0.5, 0.75, 5 / 6], $entry->getValue());
+        $this->assertSame('-0.5 0.75 ' . (string) (5 / 6), $entry->toString());
 
         $entry->setValue([[-7, -8]]);
         $this->assertSame([-7, -8], $entry->getValue(['format' => 'parsed']));
         $this->assertSame(7 / 8, $entry->getValue());
-        $this->assertSame((string) 7 / 8, $entry->toString());
+        $this->assertSame((string) (7 / 8), $entry->toString());
 
         $entry->setValue([[0, 2147483647]]);
         $this->assertSame([0, 2147483647], $entry->getValue(['format' => 'parsed']));
