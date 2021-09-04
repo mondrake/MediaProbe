76ca25f1da16d1c8d809f572e264d29a40fecb3b
mondrake <mondrake.org@gmail.com>
2021-09-04 22:43:28 +0200

    gq

--- src/Entry/ExifExposureBiasValue.php
+++ src/Entry/ExifExposureBiasValue.php
@@ -15,7 +15,22 @@ class ExifExposureBiasValue extends SignedRational
     public function toString(array $options = [])
     {
         if (($options['format'] ?? null) === 'exiftool') {
-            return $this->value[0][0] == 0 ? '0' : sprintf('%s%s/%s', $this->value[0][0] * $this->value[0][1] > 0 ? '+' : '', $this->value[0][0], $this->value[0][1]);
+            $val = $this->getValue();
+            if (isset($val)) {
+                $val *= 1.00001;    # avoid round-off errors
+                if (!$val) {
+                    return '0';
+                } elseif ((((int) $val) / $val) > 0.999) {
+                    return sprintf("%+d", (int) $val);
+                } elseif (((int) ($val * 2))/ ($val * 2) > 0.999) {
+                    return sprintf("%+d/2", (int) ($val * 2));
+                } elseif (((int) ($val * 3)) / ($val * 3) > 0.999) {
+                    return sprintf("%+d/3", (int) ($val * 3));
+                } else {
+                    return sprintf("%+.3g", $val);
+                }
+            }
+            return null;
         }
         return $this->value[0][0] == 0 ? '0' : sprintf('%s%.01f', $this->value[0][0] * $this->value[0][1] > 0 ? '+' : '', $this->value[0][0] / $this->value[0][1]);
     }
