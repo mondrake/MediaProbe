932e1c58164a750684b5fcce2e7939fbc7f88e43
mondrake <mondrake.org@gmail.com>
2021-09-05 17:31:42 +0200

    hx

--- src/Entry/Vendor/Canon/Exif/ShotInfo/AFPointsInFocus.php
+++ src/Entry/Vendor/Canon/Exif/ShotInfo/AFPointsInFocus.php
@@ -15,11 +15,11 @@ class AFPointsInFocus extends SignedShort
      */
     public function getValue(array $options = [])
     {
-        if ($alternative_af_points_in_focus = $this->getRootElement()->getElement("//makerNote[@name='Canon']/*[@name!='CanonShotInfo']/tag[@name='AFPointsInFocus']/entry")) {
-            return $alternative_af_points_in_focus->getValue($options);
-        } else {
+//        if ($alternative_af_points_in_focus = $this->getRootElement()->getElement("//makerNote[@name='Canon']/*[@name!='CanonShotInfo']/tag[@name='AFPointsInFocus']/entry")) {
+//            return $alternative_af_points_in_focus->getValue($options);
+//        } else {
             return $this->value[0];
-        }
+//        }
     }
 
     /**
