514417d107ca0b3ccf8a958f128ca493865ed3bb
mondrake <mondrake.org@gmail.com>
2021-09-05 16:50:40 +0200

    hu

--- src/Entry/Vendor/Canon/Exif/TargetExposureTime.php
+++ src/Entry/Vendor/Canon/Exif/TargetExposureTime.php
@@ -2,11 +2,15 @@
 
 namespace FileEye\MediaProbe\Entry\Vendor\Canon\Exif;
 
+use FileEye\MediaProbe\Entry\ExifTrait;
+
 /**
  * Handler for Canon Target Exposure Time tags.
  */
 class TargetExposureTime extends ExposureTime
 {
+    use ExifTrait;
+
     /**
      * {@inheritdoc}
      */
