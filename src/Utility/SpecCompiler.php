0daf2d95b878ded9cd665e46615acf98a36c3d07
mondrake <mondrake.org@gmail.com>
2021-10-16 18:21:15 +0200

    1

--- src/Utility/SpecCompiler.php
+++ src/Utility/SpecCompiler.php
@@ -220,10 +220,9 @@ DATA;
 
             // Add item to map by collection/name.
             if (isset($item['name'])) {
-//                if (!in_array($id, array_values($map['itemsByName'][$item['name']] ?? []))) {
-//                    $map['itemsByName'][$item['name']][] = $id;
-//                }
-                  $map['itemsByName'][$item['name']] = $id;
+                if (!in_array($id, array_values($map['itemsByName'][$item['name']] ?? []))) {
+                    $map['itemsByName'][$item['name']][] = $id;
+                }
             }
 
             // Add item to map by exif_read_data key.
