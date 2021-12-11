c06df47b459018ac9dfa1ddc1e6ad463c842f0fb
mondrake <mondrake.org@gmail.com>
2021-12-11 15:12:42 +0100

    20

--- src/Entry/Vendor/Canon/Exif/Functions2/AEBShotCount.php
+++ src/Entry/Vendor/Canon/Exif/Functions2/AEBShotCount.php
@@ -30,8 +30,7 @@ class AEBShotCount extends SignedLong
      */
     public function toString(array $options = [])
     {
-dump($this->getValue($options));
-        $val = implode(' ', $this->getValue($options));
-        return $this->getMappedText($val);
+        $val = $this->getValue($options);
+        return $this->getMappedText(is_array($val) ? implode(' ', $val) : $val);
     }
 }
