8fb886cec7bd3bc86349fdd33c7a26732a188399
mondrake <mondrake.org@gmail.com>
2021-12-11 19:49:21 +0100

    36

--- src/Entry/ExifLensInfo.php
+++ src/Entry/ExifLensInfo.php
@@ -15,7 +15,7 @@ class ExifLensInfo extends Rational
     public function toString(array $options = [])
     {
         if (($options['format'] ?? null) === 'exiftool') {
-            $val = $this->getValue();
+            $val = $this->getValue($options);
             if ($val[0] == $val[1]) {
               $str = $val[0];
             } else {
