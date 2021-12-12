085f3a472ec321773e0bc2a35055e9b4d4cb4dd7
mondrake <mondrake.org@gmail.com>
2021-12-12 17:31:43 +0100

    20

--- src/Entry/Vendor/Canon/Exif/Functions2/ContinuousShootingSpeed.php
+++ src/Entry/Vendor/Canon/Exif/Functions2/ContinuousShootingSpeed.php
@@ -17,8 +17,8 @@ class ContinuousShootingSpeed extends SignedLong
         $val = $this->getValue($options);
         $ret = [];
         $ret[] = $this->getMappedText($val[0]);
-        $ret[] = 'Hi ' . $val[1]);
-        $ret[] = 'Lo ' . $val[2]);
+        $ret[] = 'Hi ' . $val[1];
+        $ret[] = 'Lo ' . $val[2];
         return implode('; ', $ret);
     }
 }
