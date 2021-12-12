43afa01e7dc10decb6ecbd6daa3d13e4fb3e5c1e
mondrake <mondrake.org@gmail.com>
2021-12-12 23:08:29 +0100

    6

--- src/Entry/Vendor/Canon/Exif/ShotInfo/FNumber.php
+++ src/Entry/Vendor/Canon/Exif/ShotInfo/FNumber.php
@@ -9,14 +9,4 @@ use FileEye\MediaProbe\Entry\Vendor\Canon\Exif\ApertureValue;
  */
 class FNumber extends ApertureValue
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function toString(array $options = [])
-    {
-$model = $this->getRootElement()->getElement("//ifd[@name='IFD0']/tag[@name='Model']/entry")->getValue();
-dump([$model, $this->value, $this->getValue(), round($this->getValue(), 1), sprintf("%.2g", $this->getValue())]);
-        return sprintf("%.2g", $this->getValue());
-        //return round($this->getValue(), 1);
-    }
 }
