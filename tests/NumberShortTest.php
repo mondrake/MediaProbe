57422dae1b2b63324f0dc646fe626dba7d070351
mondrake <mondrake.org@gmail.com>
2022-02-01 22:35:38 +0100

    ax

--- tests/NumberShortTest.php
+++ tests/NumberShortTest.php
@@ -2,7 +2,6 @@
 
 namespace FileEye\MediaProbe\Test;
 
-use FileEye\MediaProbe\Data\DataString;
 use FileEye\MediaProbe\Entry\Core\Short;
 use FileEye\MediaProbe\Utility\ConvertBytes;
 
@@ -19,49 +18,9 @@ class NumberShortTest extends NumberTestCase
         $this->max = 65535;
     }
 
-    public function testOverflow()
+    protected function convertValueToBytes(int $value): string
     {
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort(0)));
-        $this->assertTrue($this->num->isParsed());
-        $this->assertSame(0, $this->num->getValue());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort($this->min - 1)));
-        $this->assertFalse($this->num->isParsed());
-        $this->assertSame(0, $this->num->getValue());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort($this->max + 1)));
-        $this->assertFalse($this->num->isParsed());
-        $this->assertSame(0, $this->num->getValue());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort(0) . ConvertBytes::fromShort($this->max + 1)));
-        $this->assertFalse($this->num->isParsed());
-        $this->assertSame([0, 0], $this->num->getValue());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort(0) . ConvertBytes::fromShort($this->min - 1)));
-        $this->assertFalse($this->num->isParsed());
-        $this->assertSame([0, 0], $this->num->getValue());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort($this->min) . ConvertBytes::fromShort($this->max)));
-        $this->assertTrue($this->num->isParsed());
-        $this->assertSame([$this->min, $this->max], $this->num->getValue());
+        return ConvertBytes::fromShort($value);
     }
 
-    public function testReturnValues()
-    {
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort(1) . ConvertBytes::fromShort(2) . ConvertBytes::fromShort(3)));
-        $this->assertSame([1, 2, 3], $this->num->getValue());
-        $this->assertSame('1 2 3', $this->num->toString());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort(1)));
-        $this->assertSame(1, $this->num->getValue());
-        $this->assertSame('1', $this->num->toString());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort($this->max)));
-        $this->assertSame($this->max, $this->num->getValue());
-        $this->assertSame((string) $this->max, $this->num->toString());
-
-        $this->num->setDataElement(new DataString(ConvertBytes::fromShort($this->min)));
-        $this->assertSame($this->min, $this->num->getValue());
-        $this->assertSame((string) $this->min, $this->num->toString());
-    }
 }
--- tests/NumberTestCase.php
+++ tests/NumberTestCase.php
@@ -2,6 +2,7 @@
 
 namespace FileEye\MediaProbe\Test;
 
+use FileEye\MediaProbe\Data\DataString;
 use FileEye\MediaProbe\MediaProbe;
 
 abstract class NumberTestCase extends EntryTestBase
@@ -12,47 +13,57 @@ abstract class NumberTestCase extends EntryTestBase
 
     public function testOverflow()
     {
-        $this->num->setDataElement([0]);
+        $this->num->setDataElement($this->toBytes([0]));
         $this->assertTrue($this->num->isParsed());
         $this->assertSame(0, $this->num->getValue());
 
-        $this->num->setDataElement([$this->min - 1]);
+        $this->num->setDataElement($this->toBytes([$this->min - 1]));
         $this->assertFalse($this->num->isParsed());
         $this->assertSame(0, $this->num->getValue());
 
-        $this->num->setDataElement([$this->max + 1]);
+        $this->num->setDataElement($this->toBytes([$this->max + 1]));
         $this->assertFalse($this->num->isParsed());
         $this->assertSame(0, $this->num->getValue());
 
-        $this->num->setDataElement([0, $this->max + 1]);
+        $this->num->setDataElement($this->toBytes([0, $this->max + 1]));
         $this->assertFalse($this->num->isParsed());
         $this->assertSame([0, 0], $this->num->getValue());
 
-        $this->num->setDataElement([0, $this->min - 1]);
+        $this->num->setDataElement($this->toBytes([0, $this->min - 1]));
         $this->assertFalse($this->num->isParsed());
         $this->assertSame([0, 0], $this->num->getValue());
 
-        $this->num->setDataElement([$this->min, $this->max]);
+        $this->num->setDataElement($this->toBytes([$this->min, $this->max]));
         $this->assertTrue($this->num->isParsed());
         $this->assertSame([$this->min, $this->max], $this->num->getValue());
     }
 
     public function testReturnValues()
     {
-        $this->num->setDataElement([1, 2, 3]);
+        $this->num->setDataElement($this->toBytes([1, 2, 3]));
         $this->assertSame([1, 2, 3], $this->num->getValue());
         $this->assertSame('1 2 3', $this->num->toString());
 
-        $this->num->setDataElement([1]);
+        $this->num->setDataElement($this->toBytes([1]));
         $this->assertSame(1, $this->num->getValue());
         $this->assertSame('1', $this->num->toString());
 
-        $this->num->setDataElement([$this->max]);
+        $this->num->setDataElement($this->toBytes([$this->max]));
         $this->assertSame($this->max, $this->num->getValue());
         $this->assertSame((string) $this->max, $this->num->toString());
 
-        $this->num->setDataElement([$this->min]);
+        $this->num->setDataElement($this->toBytes([$this->min]));
         $this->assertSame($this->min, $this->num->getValue());
         $this->assertSame((string) $this->min, $this->num->toString());
     }
+
+    protected function toBytes(array $values): string
+    {
+        $ret = '';
+        foreach ($values as $value) {
+            $ret .= $this->convertValueToBytes($value);
+        }
+        return new DataString($ret);
+    }
+
 }
