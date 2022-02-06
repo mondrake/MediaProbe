05000c82da488a9e0086ea3def077e0198f052ea
mondrake <mondrake.org@gmail.com>
2022-02-06 12:04:37 +0100

    hf

--- src/Block/Exif/Ifd.php
+++ src/Block/Exif/Ifd.php
@@ -365,7 +365,7 @@ class Ifd extends ListBase
         $ifd->setAttribute('id', 37500);
         $ifd->setAttribute('name', $maker_note_ifd_name);
         $data = $maker_note_tag->getElement("entry")->getDataElement();
-        $ifd->parseData($data, 0, null, - $data->getStart() + 12);  // @todo xxx this is incorrect, parsing should happen indepentently from add'l offset
+        $ifd->parseData($data, 0, null, - $data->getStart() + 18);  // @todo xxx this is incorrect, parsing should happen indepentently from add'l offset
 
         // Remove the MakerNote tag that has been converted to IFD.
         $exif_ifd->removeElement("tag[@name='MakerNote']");
