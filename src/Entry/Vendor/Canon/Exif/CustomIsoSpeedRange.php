7944dd0e9c8ecb7c39e3eb1fc9287d46aca15f44
mondrake <mondrake.org@gmail.com>
2022-02-06 16:44:03 +0100

    hn

--- src/Entry/Vendor/Canon/Exif/CustomIsoSpeedRange.php
+++ src/Entry/Vendor/Canon/Exif/CustomIsoSpeedRange.php
@@ -14,13 +14,16 @@ class CustomIsoSpeedRange extends SignedLong
      */
     public function getValue(array $options = [])
     {
-dump([$this->value, parent::getValue($options), parent::getValue()]);
         $format = $options['format'] ?? null;
         if ($format === 'exiftool') {
+            $value = parent::getValue();
+            if (!is_array($value)) {
+                return $value;
+            }
             $v = [];
-            $v[0] = parent::getValue()[0];
-            $v[1] = parent::getValue()[1] < 2 ? parent::getValue()[1] : (parent::getValue()[1] < 1000 ? exp((parent::getValue()[1] / 8 - 9) * log(2)) * 100 : 0);
-            $v[2] = parent::getValue()[2] < 2 ? parent::getValue()[2] : (parent::getValue()[2] < 1000 ? exp((parent::getValue()[2] / 8 - 9) * log(2)) * 100 : 0);
+            $v[0] = $value[0];
+            $v[1] = $value[1] < 2 ? $value[1] : ($value[1] < 1000 ? exp(($value[1] / 8 - 9) * log(2)) * 100 : 0);
+            $v[2] = $value[2] < 2 ? $value[2] : ($value[2] < 1000 ? exp(($value[2] / 8 - 9) * log(2)) * 100 : 0);
             return $v;
         }
         return parent::getValue($options);
@@ -34,6 +37,9 @@ dump([$this->value, parent::getValue($options), parent::getValue()]);
         $format = $options['format'] ?? null;
         if ($format === 'exiftool') {
             $val = $this->getValue($options);
+            if (!is_array($val)) {
+                return 'n/a';
+            }
             $str = $val[0] === 0 ? 'Disable; Max ' : 'Enable; Max ';
             $str .= $val[1] . '; Min ' . $val[2];
             return $str;
