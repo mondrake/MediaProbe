dab28d3b58561fc420efb73f3cc55c7b3dd8d393
mondrake <mondrake.org@gmail.com>
2022-02-06 15:11:45 +0100

    hg

--- src/Block/Exif/Ifd.php
+++ src/Block/Exif/Ifd.php
@@ -365,6 +365,7 @@ class Ifd extends ListBase
         $ifd->setAttribute('id', 37500);
         $ifd->setAttribute('name', $maker_note_ifd_name);
         $data = $maker_note_tag->getElement("entry")->getDataElement();
+dump([$model, $data->getStart()]);        
         $ifd->parseData($data, 0, null, - $data->getStart() + 30);  // @todo xxx this is incorrect, parsing should happen indepentently from add'l offset
 
         // Remove the MakerNote tag that has been converted to IFD.
