b12d6119039fcddb30818f6b76a531aede692640
mondrake <mondrake.org@gmail.com>
2022-02-09 17:04:14 +0100

    ai

---
+++ src/Entry/IfdApplicationNotes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FileEye\MediaProbe\Entry;
+
+use FileEye\MediaProbe\Entry\Core\Char;
+
+/**
+ * Decode text for an Ifd/ApplicationNotes tag.
+ */
+class IfdApplicationNotes extends Char
+{
+}
