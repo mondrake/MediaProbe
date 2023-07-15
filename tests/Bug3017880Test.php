75c57bb19d731a81e052252c44381e69db4e27c0
mondrake <mondrake.org@gmail.com>
2023-07-15 17:55:34 +0200

    10

--- tests/Bug3017880Test.php
+++ tests/Bug3017880Test.php
@@ -31,10 +31,7 @@ class Bug3017880Test extends MediaProbeTestCaseBase
             // branding is an original)
 
             if ($exif === null) {
-                $app1_segment_mock = $this->getMockBuilder('FileEye\MediaProbe\Block\JpegSegmentApp1')
-                    ->disableOriginalConstructor()
-                    ->getMock();
-
+                $app1_segment_mock = new StubRootBlock($this->createMock(ItemDefinition::class));
                 $exif_definition = new ItemDefinition(CollectionFactory::get('Exif\Exif'));
                 $exif = new Exif($exif_definition, $app1_segment_mock);
                 $tiff_definition = new ItemDefinition(CollectionFactory::get('Tiff\Tiff'));
