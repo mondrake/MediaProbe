91900473ea32230cf3abb4056a8f91a4e0d44819
mondrake <mondrake.org@gmail.com>
2024-01-21 20:12:06 +0000

    x

---
+++ src/Collection/MediaType.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is generated automatically by executing the 'fileeye-mediaprobe compile' command.
+ *
+ * DO NOT CHANGE MANUALLY.
+ */
+// phpcs:disable
+
+namespace FileEye\MediaProbe\Collection;
+
+use FileEye\MediaProbe\Collection\CollectionBase;
+
+class MediaType extends CollectionBase {
+
+  protected static $map = array (
+  'class' => 'FileEye\\MediaProbe\\MediaType',
+  'id' => 'MediaType',
+  'items' =>
+  array (
+    'image/jpeg' =>
+    array (
+      0 =>
+      array (
+        'collection' => 'Jpeg\\Jpeg',
+      ),
+    ),
+    'image/tiff' =>
+    array (
+      0 =>
+      array (
+        'collection' => 'Tiff\\Tiff',
+      ),
+    ),
+  ),
+);
+}
