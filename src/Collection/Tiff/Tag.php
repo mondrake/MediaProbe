a68e54f0566f8c4516959691a93c7576a90d31bd
mondrake <mondrake.org@gmail.com>
2024-01-24 18:20:53 +0100

    compile

---
+++ src/Collection/Tiff/Tag.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is generated automatically by executing the 'fileeye-mediaprobe compile' command.
+ *
+ * DO NOT CHANGE MANUALLY.
+ */
+// phpcs:disable
+
+namespace FileEye\MediaProbe\Collection\Tiff;
+
+use FileEye\MediaProbe\Collection\CollectionBase;
+
+class Tag extends CollectionBase {
+
+  protected static $map = array (
+  'title' => 'Tag',
+  'class' => 'FileEye\\MediaProbe\\Block\\Tiff\\Tag',
+  'DOMNode' => 'tag',
+  'id' => 'Tiff\\Tag',
+);
+}
---
+++ src/Collection/Tiff/UnknownTag.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is generated automatically by executing the 'fileeye-mediaprobe compile' command.
+ *
+ * DO NOT CHANGE MANUALLY.
+ */
+// phpcs:disable
+
+namespace FileEye\MediaProbe\Collection\Tiff;
+
+use FileEye\MediaProbe\Collection\CollectionBase;
+
+class UnknownTag extends CollectionBase {
+
+  protected static $map = array (
+  'title' => 'Unknown Tag',
+  'class' => 'FileEye\\MediaProbe\\Block\\Tiff\\Tag',
+  'DOMNode' => 'tag',
+  'id' => 'Tiff\\UnknownTag',
+);
+}
