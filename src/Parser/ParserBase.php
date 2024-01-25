d62f70af6fced50f0278052b8f4487e2a3d23323
mondrake <mondrake.org@gmail.com>
2024-01-25 10:34:04 +0100

    d

--- src/Parser/ParserBase.php
+++ src/Parser/ParserBase.php
@@ -5,7 +5,7 @@ namespace FileEye\MediaProbe\Parser;
 use FileEye\MediaProbe\Data\DataElement;
 use FileEye\MediaProbe\Model\BlockInterface;
 
-class ParserBase
+abstract class ParserBase
 {
     public function __construct(
         protected readonly BlockInterface $block,
