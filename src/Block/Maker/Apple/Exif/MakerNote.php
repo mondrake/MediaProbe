051d0adaf7e94c7cbcd67458512901d3056783e3
mondrake <mondrake.org@gmail.com>
2025-03-29 15:55:17 +0100

    v

--- src/Block/Maker/Apple/Exif/MakerNote.php
+++ src/Block/Maker/Apple/Exif/MakerNote.php
@@ -11,7 +11,6 @@ use FileEye\MediaProbe\Data\DataElement;
 use FileEye\MediaProbe\Data\DataException;
 use FileEye\MediaProbe\Data\DataFormat;
 use FileEye\MediaProbe\Data\DataWindow;
-use FileEye\MediaProbe\ItemDefinition;
 use FileEye\MediaProbe\MediaProbeException;
 use FileEye\MediaProbe\Model\ListBase;
 use FileEye\MediaProbe\Model\ListItemValue;
